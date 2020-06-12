703818204178fc2c42927d2a1d7618bc77f6b645
Sergey Ivanov <voivoid@mail.ru>
2020-06-12 11:52:15 +0300

    add floating point types

--- lib/src/watchme/numvar.cpp
+++ lib/src/watchme/numvar.cpp
@@ -1,4 +1,4 @@
-#include "watchme/intvar.h"
+#include "watchme/numvar.h"
 
 #include "imgui.h"
 
@@ -24,13 +24,31 @@ constexpr size_t count_digits()
   return digits + sign;
 }
 
+// TODO: figure out max digits num
+
+template <>
+constexpr size_t count_digits<float>()
+{
+  return 256;
+}
+template <>
+constexpr size_t count_digits<double>()
+{
+  return 256;
+}
+template <>
+constexpr size_t count_digits<long double>()
+{
+  return 256;
+}
+
 template <typename T>
 const char* const type_name;
 
 #define DEFINE_TYPE( type )                                                                                                                \
   template <>                                                                                                                              \
   const char* const type_name<type> = #type;                                                                                               \
-  template watch_me::IntegerVar<type>
+  template watch_me::NumericVar<type>
 
 DEFINE_TYPE( short );
 DEFINE_TYPE( unsigned short );
@@ -40,6 +58,9 @@ DEFINE_TYPE( long );
 DEFINE_TYPE( unsigned long );
 DEFINE_TYPE( long long );
 DEFINE_TYPE( unsigned long long );
+DEFINE_TYPE( float );
+DEFINE_TYPE( double );
+DEFINE_TYPE( long double );
 
 
 }  // namespace
@@ -47,15 +68,17 @@ DEFINE_TYPE( unsigned long long );
 namespace watch_me
 {
 template <typename T>
-IntegerVar<T>::IntegerVar( const char* const name, const T* const ptr ) : Var( name, ptr ), var_ptr( ptr )
+NumericVar<T>::NumericVar( const char* const name, const T* const ptr ) : Var( name, ptr ), var_ptr( ptr )
 {
 }
 
 template <typename T>
-void IntegerVar<T>::draw_frame_impl()
+void NumericVar<T>::draw_frame_impl()
 {
   char buff[ count_digits<T>() + 1 ];
-  *std::to_chars( std::begin( buff ), std::end( buff ) - 1, *var_ptr ).ptr = '\0';
+  auto [ ptr, err ] = std::to_chars( std::begin( buff ), std::end( buff ) - 1, *var_ptr );
+  *ptr              = '\0';
+  assert( err == std::errc{} );
 
   ImGui::InputText( type_name<T>, buff, std::size( buff ) );
 }
--- samples/main.cpp
+++ samples/main.cpp
@@ -7,10 +7,12 @@ int main()
 {
   int x           = 0;
   long long y     = 0;
+  double d        = 0.0;
   std::string str = "hello";
 
   WATCH_ME( x );
   WATCH_ME( y );
+  WATCH_ME( d );
   WATCH_ME( str );
 
   for ( size_t i = 0; i < 10; ++i )
@@ -18,6 +20,7 @@ int main()
     std::this_thread::sleep_for( std::chrono::seconds( 1 ) );
     x += 10;
     y += 20;
+    d += 1.11;
     str += '!';
   }
 
