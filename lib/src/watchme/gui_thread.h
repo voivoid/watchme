1e61f74beac271bf7af3ccaf680fb4e9dcadd496
Sergey Ivanov <sergey.ivanov@nordigy.ru>
2020-09-06 22:18:11 +0300

    use std packaged_task instead of std promise

--- lib/src/watchme/gui_thread.h
+++ lib/src/watchme/gui_thread.h
@@ -4,9 +4,8 @@
 
 namespace watch_me
 {
-using GuiTask = std::function<void( void )>;
-void send_gui_task( GuiTask task );
-void post_gui_task( GuiTask task );
+void send_gui_task( std::function<void()> task );
+void post_gui_task( std::function<void()> task );
 
 void assert_this_is_gui_thread();
 }  // namespace watch_me
