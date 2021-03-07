7110b882825f8967322795f985f665c17d4038b2
Sergey Ivanov <sergey.ivanov@nordigy.ru>
2021-03-07 18:26:17 +0300

    update to imgui 1.81

--- cmake/thirdparties/imgui.cmake
+++ cmake/thirdparties/imgui.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
   get_imgui
   GIT_REPOSITORY https://github.com/ocornut/imgui
-  GIT_TAG        v1.76
+  GIT_TAG        v1.81
 )
 
 FetchContent_GetProperties(get_imgui)
@@ -16,6 +16,7 @@ if(NOT get_imgui_POPULATED)
                        ${SrcDir}/imgui.h
                        ${SrcDir}/imgui_draw.cpp
                        ${SrcDir}/imgui_widgets.cpp
+                       ${SrcDir}/imgui_tables.cpp
                        ${SrcDir}/imgui_internal.h
                        ${SrcDir}/imstb_rectpack.h
                        ${SrcDir}/imstb_textedit.h
@@ -25,14 +26,14 @@ if(NOT get_imgui_POPULATED)
   target_include_directories(ImGuiLib PUBLIC ${SrcDir})
 
   if(WIN32)
-    add_library(ImGuiLibPlatform ${SrcDir}/examples/imgui_impl_win32.h
-                                 ${SrcDir}/examples/imgui_impl_win32.cpp
-                                 ${SrcDir}/examples/imgui_impl_dx9.h
-                                 ${SrcDir}/examples/imgui_impl_dx9.cpp)
+    add_library(ImGuiLibPlatform ${SrcDir}/backends/imgui_impl_win32.h
+                                 ${SrcDir}/backends/imgui_impl_win32.cpp
+                                 ${SrcDir}/backends/imgui_impl_dx9.h
+                                 ${SrcDir}/backends/imgui_impl_dx9.cpp)
     add_library(ImGui::Platform ALIAS ImGuiLibPlatform)
 
     target_link_libraries(ImGuiLibPlatform PUBLIC ImGuiLib d3d9.lib)
-    target_include_directories(ImGuiLibPlatform PUBLIC ${SrcDir}/examples)
+    target_include_directories(ImGuiLibPlatform PUBLIC ${SrcDir}/backends)
   else()
     message(ERROR "Unknown platform")
   endif()
